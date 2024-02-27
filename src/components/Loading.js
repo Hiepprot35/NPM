bd4113f4419e022d8e2cb59dde52b89ee9c58807
Tuan Hiep <tansou57@gmail.com>
2024-02-27 16:37:00 +0700

    ok

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,21 +1,36 @@
-
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { useEffect, useState } from 'react';
 
-export const IsLoading = () => {
-    return (
+const IsLoading = () => {
+  const [internetError, setInternetError] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInternetError(true);
+    }, 10000);
+  }, []);
+
+  return (
+    <>
+      {internetError ? (
+        <p>Lỗi kết nối từ Server</p>
+      ) : (
         <div className="Loading_container">
-
-            <div className="">
+          <div className="">
             <Player
-  autoplay
-  speed={1}	
-  loop
-  src="https://lottie.host/c775397d-d0b4-434c-b084-489acbe2d17b/CpkFsQb8HX.json"
-  style={{ height: '300px', width: '300px' }}
->
-  <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
-</Player>
-            </div>
+              autoplay
+              speed={1}
+              loop
+              src="https://lottie.host/c775397d-d0b4-434c-b084-489acbe2d17b/CpkFsQb8HX.json"
+              style={{ height: '300px', width: '300px' }}
+            >
+              <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+            </Player>
+          </div>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
+
+export default IsLoading;
